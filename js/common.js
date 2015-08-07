1e240401907cfd90193df5dd7ae50809387e4bc5
Evgeniy Beskrovnyy <e.beskrovnyy@gmail.com>
2015-08-07 16:19:04 +0300

    popup nav cross animate

--- js/common.js
+++ js/common.js
@@ -2,6 +2,18 @@
 	$(function() {
 
 		document.getElementById('svg-icons').innerHTML = SVG_ICONS;
- 
+ 		
+ 		animatePopupNav (); //-- анимация всплывающего меню
 		
 	});
+
+
+	function animatePopupNav() {
+		$(".nav-ctrl").on('click', function() {
+			event.preventDefault();
+			
+			$(this).toggleClass('is-active');
+
+		});
+
+	} // animatePopupNav ()
