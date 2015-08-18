66659673c988d60e12f528d816231590f9b51a92
Evgeniy Beskrovnyy <e.beskrovnyy@gmail.com>
2015-08-18 22:30:44 +0300

    svg for add button

--- js/svg.js
+++ js/svg.js
@@ -14,24 +14,25 @@ var SVG_ICONS =
 '="currentColor" d="M10 13c0 .553-.447 1-1 1H1c-.552 0-1-.447-1-1V1c0-.552.448-1 1-1h8c.553 0 1 .448 1 1v12z"/><path f'+
 'ill="#fff" d="M1 1h8v10H1zM4 12.167h2v.666H4z"/></symbol><symbol viewBox="0 0 16 16" id="icon_next-small"><title>next'+
 '-small</title><path fill="currentColor" d="M4.097 16l-.43-.44L11.453 8 3.668.436 4.098 0l8.234 8"/></symbol><symbol v'+
-'iewBox="0 0 16 16" id="icon_previous-small"><title>previous-small</title><path fill="currentColor" d="M3.668 8l8.235-'+
-'8 .43.437L4.547 8l7.784 7.56-.43.44"/></symbol><symbol viewBox="0 0 16 16" id="icon_search"><title>search</title><pat'+
-'h fill="currentColor" d="M15.828 15l-3.875-3.875c.99-1.178 1.59-2.695 1.59-4.354C13.542 3.033 10.51 0 6.77 0S0 3.032 '+
-'0 6.77c0 3.74 3.032 6.772 6.77 6.772 1.66 0 3.178-.598 4.355-1.59L15 15.83c.23.23.6.23.83 0 .227-.228.227-.6-.002-.82'+
-'8zM6.77 12.312c-3.06 0-5.54-2.48-5.54-5.54s2.48-5.54 5.54-5.54c3.062 0 5.54 2.48 5.54 5.54s-2.48 5.54-5.54 5.54z"/></'+
-'symbol><symbol viewBox="3 -5 16 16" id="icon_star"><title>star</title><path class="icon-star" d="M18.5 2l-3.1 3 .7 4.'+
-'3c0 .3-.1.6-.3.8-.1.1-.3.2-.5.2-.1 0-.3 0-.4-.1l-3.9-2-3.9 2c-.3.1-.6.1-.9 0-.2-.2-.3-.5-.2-.8l.7-4.3-3.1-3c-.2-.2-.3'+
-'-.6-.2-.9s.3-.5.6-.5L8.3 0l1.9-3.9c.1-.3.4-.5.7-.5.3 0 .6.2.7.5l2 3.9 4.3.6c.3 0 .6.3.7.6.1.3.1.6-.1.8z"/></symbol><s'+
-'ymbol viewBox="0 0 14 13" id="icon_teasers-view-list"><title>teasers-view-list</title><path fill="currentColor" d="M0'+
-' 0h3v3H0zM4 0h9v3H4zM0 5h3v3H0zM4 5h9v3H4zM0 10h3v3H0zM4 10h9v3H4z"/></symbol><symbol viewBox="0 0 14 13" id="icon_te'+
-'asers-view-tile"><title>teasers-view-tile</title><g fill="currentColor"><path d="M0 0h6v6H0z"/><path d="M0 0h6v6H0zM7'+
-' 0h6v6H7zM0 7h6v6H0z"/><path d="M0 7h6v6H0zM7 7h6v6H7z"/></g></symbol><symbol viewBox="0 0 32 32" id="icon_user"><tit'+
-'le>user</title><path fill="currentColor" d="M16 22c4.963 0 9-4.936 9-11S20.962 0 16 0c-2.447 0-4.734 1.174-6.438 3.30'+
-'5C7.91 5.37 7 8.105 7 11c0 6.064 4.037 11 9 11zm0-20c3.86 0 7 4.038 7 9 0 4.963-3.14 9-7 9s-7-4.037-7-9c0-4.962 3.14-'+
-'9 7-9zm7 18c-.553 0-1 .447-1 1s.447 1 1 1c3.86 0 7 3.14 7 7 0 .55-.45 1-1 1H3c-.55 0-1-.45-1-1 0-3.86 3.14-7 7-7 .553'+
-' 0 1-.447 1-1s-.447-1-1-1c-4.963 0-9 4.037-9 9 0 1.654 1.346 3 3 3h26c1.654 0 3-1.346 3-3 0-4.962-4.037-9-9-9z"/></sy'+
-'mbol><symbol viewBox="0 0 16 16" id="icon_warning"><title>warning</title><g fill="currentColor"><path d="M8 0C3.58 0 '+
-'0 3.58 0 8c0 4.417 3.58 8 8 8s8-3.582 8-8c0-4.42-3.58-8-8-8zm0 14.695c-3.698 0-6.695-2.997-6.695-6.696S4.302 1.302 8 '+
-'1.302c3.698 0 6.695 3 6.695 6.698 0 3.698-2.997 6.695-6.695 6.695z"/><path d="M8.85 9.73l.326-6.696H6.84l.326 6.696M8'+
-'.007 10.468c-.783 0-1.33.548-1.33 1.347 0 .768.532 1.344 1.3 1.344h.03c.797 0 1.315-.577 1.315-1.345-.013-.8-.53-1.34'+
-'7-1.315-1.347z"/></g></symbol></svg>';
+'iewBox="0 0 10 10" id="icon_plus"><title>plus</title><path fill="currentColor" d="M10 4H6V0H4v4H0v2h4v4h2V6h4"/></sym'+
+'bol><symbol viewBox="0 0 16 16" id="icon_previous-small"><title>previous-small</title><path fill="currentColor" d="M3'+
+'.668 8l8.235-8 .43.437L4.547 8l7.784 7.56-.43.44"/></symbol><symbol viewBox="0 0 16 16" id="icon_search"><title>searc'+
+'h</title><path fill="currentColor" d="M15.828 15l-3.875-3.875c.99-1.178 1.59-2.695 1.59-4.354C13.542 3.033 10.51 0 6.'+
+'77 0S0 3.032 0 6.77c0 3.74 3.032 6.772 6.77 6.772 1.66 0 3.178-.598 4.355-1.59L15 15.83c.23.23.6.23.83 0 .227-.228.22'+
+'7-.6-.002-.828zM6.77 12.312c-3.06 0-5.54-2.48-5.54-5.54s2.48-5.54 5.54-5.54c3.062 0 5.54 2.48 5.54 5.54s-2.48 5.54-5.'+
+'54 5.54z"/></symbol><symbol viewBox="3 -5 16 16" id="icon_star"><title>star</title><path class="icon-star" d="M18.5 2'+
+'l-3.1 3 .7 4.3c0 .3-.1.6-.3.8-.1.1-.3.2-.5.2-.1 0-.3 0-.4-.1l-3.9-2-3.9 2c-.3.1-.6.1-.9 0-.2-.2-.3-.5-.2-.8l.7-4.3-3.'+
+'1-3c-.2-.2-.3-.6-.2-.9s.3-.5.6-.5L8.3 0l1.9-3.9c.1-.3.4-.5.7-.5.3 0 .6.2.7.5l2 3.9 4.3.6c.3 0 .6.3.7.6.1.3.1.6-.1.8z"'+
+'/></symbol><symbol viewBox="0 0 14 13" id="icon_teasers-view-list"><title>teasers-view-list</title><path fill="curren'+
+'tColor" d="M0 0h3v3H0zM4 0h9v3H4zM0 5h3v3H0zM4 5h9v3H4zM0 10h3v3H0zM4 10h9v3H4z"/></symbol><symbol viewBox="0 0 14 13'+
+'" id="icon_teasers-view-tile"><title>teasers-view-tile</title><g fill="currentColor"><path d="M0 0h6v6H0z"/><path d="'+
+'M0 0h6v6H0zM7 0h6v6H7zM0 7h6v6H0z"/><path d="M0 7h6v6H0zM7 7h6v6H7z"/></g></symbol><symbol viewBox="0 0 32 32" id="ic'+
+'on_user"><title>user</title><path fill="currentColor" d="M16 22c4.963 0 9-4.936 9-11S20.962 0 16 0c-2.447 0-4.734 1.1'+
+'74-6.438 3.305C7.91 5.37 7 8.105 7 11c0 6.064 4.037 11 9 11zm0-20c3.86 0 7 4.038 7 9 0 4.963-3.14 9-7 9s-7-4.037-7-9c'+
+'0-4.962 3.14-9 7-9zm7 18c-.553 0-1 .447-1 1s.447 1 1 1c3.86 0 7 3.14 7 7 0 .55-.45 1-1 1H3c-.55 0-1-.45-1-1 0-3.86 3.'+
+'14-7 7-7 .553 0 1-.447 1-1s-.447-1-1-1c-4.963 0-9 4.037-9 9 0 1.654 1.346 3 3 3h26c1.654 0 3-1.346 3-3 0-4.962-4.037-'+
+'9-9-9z"/></symbol><symbol viewBox="0 0 16 16" id="icon_warning"><title>warning</title><g fill="currentColor"><path d='+
+'"M8 0C3.58 0 0 3.58 0 8c0 4.417 3.58 8 8 8s8-3.582 8-8c0-4.42-3.58-8-8-8zm0 14.695c-3.698 0-6.695-2.997-6.695-6.696S4'+
+'.302 1.302 8 1.302c3.698 0 6.695 3 6.695 6.698 0 3.698-2.997 6.695-6.695 6.695z"/><path d="M8.85 9.73l.326-6.696H6.84'+
+'l.326 6.696M8.007 10.468c-.783 0-1.33.548-1.33 1.347 0 .768.532 1.344 1.3 1.344h.03c.797 0 1.315-.577 1.315-1.345-.01'+
+'3-.8-.53-1.347-1.315-1.347z"/></g></symbol></svg>';
