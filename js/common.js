ecf6e504787c72dcb1599ad5558b8f0216df6389
Evgeniy Beskrovnyy <e.beskrovnyy@gmail.com>
2015-09-02 16:56:42 +0300

    cabinet - list of adds is done

--- js/common.js
+++ js/common.js
@@ -230,6 +230,44 @@
                 removalDelay: 300,
                 mainClass: 'my-mfp-slide-bottom'
             });
+
+        // Подключение плагина для увеличения и просмотра изображений
+        var zoomClass = 'js-img-zoom';
+        $('.'+ zoomClass).magnificPopup({
+            gallery:{enabled:true},
+            delegate: '.' + zoomClass + '__target', // child items selector, by clicking on it popup will open
+            type: 'image',
+
+            image: {
+                titleSrc: function(item) {
+                    var imgDate = item.el.attr('data-date');
+                    var imgTitle = item.el.attr('title');
+                    if (imgDate === undefined) {imgDate = '';}
+                    if (imgTitle === undefined) {imgTitle = '';}
+                    var imgCapture = '<small class="mfp-title__date">' + imgDate + '</small>' + imgTitle;
+                    return imgCapture;
+                }
+            },
+
+            mainClass: 'mfp-with-zoom', // this class is for CSS animation below
+
+              zoom: {
+                enabled: false, // By default it's false, so don't forget to enable it
+
+                duration: 300, // duration of the effect, in milliseconds
+                easing: 'ease-in-out', // CSS transition easing function 
+
+                // The "opener" function should return the element from which popup will be zoomed in
+                // and to which popup will be scaled down
+                // By defailt it looks for an image tag:
+                opener: function(openerElement) {
+                  // openerElement is the element on which popup was initialized, in this case its <a> tag
+                  // you don't need to add "opener" option if this code matches your needs, it's defailt one.
+                  return openerElement.is('img') ? openerElement : openerElement.find('img');
+                }
+              }
+
+        });
         
 
         // кастомизация выбор города
